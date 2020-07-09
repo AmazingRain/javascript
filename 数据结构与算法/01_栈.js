6592ed27f315563f489ef812363c70e48b2e7df8
木子雨 <2186344394@qq.com>
2020-07-09 20:31:26 +0800

    十进制数转二进制数的拓展

--- 数据结构与算法/01_栈.js
+++ 数据结构与算法/01_栈.js
@@ -42,3 +42,31 @@ function decimalToBinary(decNum) {
   }
   return +stack.items.reverse().join('');
 }
+
+// 10进制转2 到36进制中的一种
+function baseChange(decNum, base) {
+  const stack = new Stack();
+  // 先将其保存，随后根据栈中的值进行预期对应，转换为对应进制
+  const digist = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let str = '';
+  let rem;
+  if (base < 2 || base > 36) {
+    return '进制输入不和法';
+  }
+
+  while (decNum > 0) {
+    rem = Math.floor(decNum % base);
+    stack.push(rem);
+    decNum = Math.floor(decNum / base);
+  }
+
+  while (stack.items.length != 0) {
+    str += digist[stack.pop()];
+  }
+
+  return str;s
+}
+
+console.log(baseChange(10, 2));  //1010
+
+
