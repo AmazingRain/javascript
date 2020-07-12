d28dec8c4ea6ccff26c8e5eebcb3585f354db50e
木子雨 <2186344394@qq.com>
2020-07-12 11:58:27 +0800

    链表剩余方法的补充

--- 数据结构与算法/03_链表.js
+++ 数据结构与算法/03_链表.js
@@ -43,4 +43,99 @@ class LinkList {
     return str;
   }
 
+  insert(data, index) {
+    if (index < 0 || index > this.length) {
+      return false;
+    }
+    const newNode = new Node(data);
+    if (index == 0) {
+      const temp = this.head;
+      this.head = newNode;
+      this.head.next = temp;
+    } else {
+      const previous = this.getNode(index);
+      const temp = previous.next;
+      newNode.next = temp;
+      previous.next = newNode;
+    }
+    this.length ++;
+  }
+  // 根据位置返回指定的元素
+  getNode(position) {
+    // 越界处理
+    if (position < 0 || position > this.length) {
+      return null;
+    }
+    let temp = this.head;
+    let index = 0;
+    while (index < position) {
+      temp = temp.next;
+      index ++;
+    }
+    return temp;
+  }
+  indexOf(data) {
+    let temp = this.head;
+    let index = 0;
+    while (temp) {
+      if (temp.data == data) {
+        return index;
+      }
+      temp = temp.next;
+      index ++;
+    }
+    return -1;
+  }
+  
+  updateByData(data, newData) {
+    let temp = this.head;
+    while (temp) {
+      if (temp.data == data) {
+        temp.data = newData;
+        return temp;
+      }
+      temp = temp.next;
+    }
+    return false;
+  }
+
+  updateByPositios(position, newData) {
+    if (position < 0 || position > this.length) {
+      return false;
+    }
+    let index = 0;
+    let temp = this.head;
+    while (index < position) {
+      temp = temp.next;
+      index ++;
+    }
+    temp.data = newData;
+    return temp;
+  }
+
+  removeByPosition(position) {
+    if (position < 0 || position > this.length) {
+      return false;
+    }
+
+    if (position == 0) {
+      this.head = this.head.next;
+    } else {
+      let previous = this.getNode(position - 1);
+      previous.next = previous.next.next;
+    }
+    this.length --;
+    return true;
+  }
+  
+  removeByData(data) {
+    let position = this.indexOf(data);
+    return this.removeByPosition(position);
+  }
+  isEmpty() {
+    return this.length == 0;
+  }
+  size() {
+    return this.length;
+  }
 }
